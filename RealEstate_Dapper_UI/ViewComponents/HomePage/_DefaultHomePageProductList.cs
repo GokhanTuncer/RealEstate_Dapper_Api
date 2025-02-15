505b86291bb2167f59c8a28d343a977b8d5d82e2
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-15 14:46:57 +0300

    Apinin consume edilmesi

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.ProductDTOs;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultHomePageProductList :ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultHomePageProductList(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44382/api/Products/ProductListWithCategory");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData= await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTOs>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+
     }
 }
