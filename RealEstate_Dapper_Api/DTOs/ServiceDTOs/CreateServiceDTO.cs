3fac8913e196dbdf4763c7aea041fc812775e465
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-17 14:23:42 +0300

    Service dtoları eklendi

---
+++ RealEstate_Dapper_Api/DTOs/ServiceDTOs/CreateServiceDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_Dapper_Api.DTOs.ServiceDTOs
+{
+    public class CreateServiceDTO
+    {
+        public string ServiceName { get; set; }
+        public bool ServiceStatus { get; set; }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/ServiceRepository/IServiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.DTOs.ServiceDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
+{
+    public interface IServiceRepository
+    {
+        Task<List<ResultServiceDTO>> GetAllServiceAsync();
+        void CreateService(CreateServiceDTO createserviceDTO);
+        void DeleteService(int id);
+        void UpdateService(UpdateServiceDTO updateserviceDTO);
+        Task<GetByIDServiceDTO> GetService(int id);
+    }
+}
