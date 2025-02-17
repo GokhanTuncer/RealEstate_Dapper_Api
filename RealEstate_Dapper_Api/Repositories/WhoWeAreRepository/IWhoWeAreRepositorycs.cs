04e50fefcfead8ded1735f0a39a94c34272b8733
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-17 12:07:30 +0300

    Biz kimiz detay kısmı eklendi

---
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreRepository/IWhoWeAreRepositorycs.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.DTOs.CategoryDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.WhoWeAreRepository
+{
+	public interface IWhoWeAreRepositorycs
+	{
+		Task<List<ResultCategoryDTO>> GetAllCategoryAsync();
+		void CreateCategory(CreateCategoryDTO categoryDTO);
+		void DeleteCategory(int id);
+		void UpdateCategory(UpdateCategoryDTO categoryDTO);
+		Task<GetByIDCategoryDTO> GetCategory(int id);
+	}
+}
