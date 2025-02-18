d46c0d88846ab4bd7f4270cafa4e0547b1639955
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-18 14:04:00 +0300

    BottomGrid Repository eklendi ve düzenlemesi yapıldı

---
+++ RealEstate_Dapper_Api/Repositories/BottomGridRepository/BottomGridRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.DTOs.BottomGridDTOs;
+using RealEstate_Dapper_Api.DTOs.ProductDTOs;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.BottomGridRepository
+{
+	public class BottomGridRepository : IBottomGridRepository
+	{
+		private readonly Context _context;
+
+		public BottomGridRepository(Context context)
+		{
+			_context = context;
+		}
+		
+		public Task CreateBottomGrid(CreateBottomGridDTO createBottomGridDTO)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteBottomGrid(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<List<ResultBottomGridDTO>> GetAllBottomGridAsync()
+		{
+			string query = "SELECT * FROM BottomGrid";
+			using (var connection = _context.CreateConnection())
+			{
+				var values = await connection.QueryAsync<ResultBottomGridDTO>(query);
+				return values.ToList();
+			}
+		}
+
+		public Task<GetBottomGridDTO> GetBottomGrid(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateBottomGrid(UpdateBottomGridDTO updateBottomGridDTO)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ RealEstate_Dapper_Api/Repositories/BottomGridRepository/IBottomGridRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.DTOs.BottomGridDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.BottomGridRepository
+{
+	public interface IBottomGridRepository
+	{
+		Task<List<ResultBottomGridDTO>> GetAllBottomGridAsync();
+		Task CreateBottomGrid(CreateBottomGridDTO createBottomGridDTO);
+		Task DeleteBottomGrid(int id);
+		Task UpdateBottomGrid(UpdateBottomGridDTO updateBottomGridDTO);
+		Task<GetBottomGridDTO> GetBottomGrid(int id);
+	}
+}
