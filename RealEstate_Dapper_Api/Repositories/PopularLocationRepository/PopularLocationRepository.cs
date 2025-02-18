474e03ef2a74f461584554341642364824d2fcc6
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-18 15:52:26 +0300

    Popular Location repository ,dto ve controller eklendi.

---
+++ RealEstate_Dapper_Api/Repositories/PopularLocationRepository/PopularLocationRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.DTOs.PopularLocationDTOs;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepository
+{
+    public class PopularLocationRepository : IPopularLocationRepository
+    {
+        private readonly Context _context;
+
+        public PopularLocationRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultPopularLocationDTO>> GetAllPopularLocationAsync()
+        {
+            string query = "SELECT * FROM PopularLocation";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultPopularLocationDTO>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
