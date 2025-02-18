8f63946e72fd71af3b4aa3c9584f1ad06df27a11
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-18 16:05:44 +0300

    Popular locations UI tarafına apiden consume edildi

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultProductListExploreCitiesComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultProductListExploreCitiesComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.PopularLocationDTOs;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultProductListExploreCitiesComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultProductListExploreCitiesComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44382/api/PopularLocations");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDTOs>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
