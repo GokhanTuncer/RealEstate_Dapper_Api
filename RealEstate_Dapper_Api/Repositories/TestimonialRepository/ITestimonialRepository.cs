76a318efef5194ec9076332aa74da0b7516fa01a
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-19 13:52:19 +0300

    Testimonial için api'ye controller, dto ve repository eklendi

---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepository/ITestimonialRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate_Dapper_Api.DTOs.TestimonialDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepository
+{
+    public interface ITestimonialRepository
+    {
+        Task<List<ResultTestimonialDTO>> GetAllTestimonialAsync();
+
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepository/TestimonialRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.DTOs.BottomGridDTOs;
+using RealEstate_Dapper_Api.DTOs.TestimonialDTOs;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepository
+{
+    public class TestimonialRepository : ITestimonialRepository
+    {
+        private readonly Context _context;
+        public TestimonialRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultTestimonialDTO>> GetAllTestimonialAsync()
+        {
+            string query = "Select * From Testimonial";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultTestimonialDTO>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
