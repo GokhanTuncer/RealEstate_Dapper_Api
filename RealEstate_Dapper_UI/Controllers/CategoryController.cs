fe796142866da904dadf9ea59c26af905392bca0
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-21 12:25:13 +0300

    Admin kategori oluşturma sayfası consume edildi

--- RealEstate_Dapper_UI/Controllers/CategoryController.cs
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RealEstate_Dapper_UI.DTOs.CategoryDTOs;
+using System.Text;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
@@ -22,6 +23,24 @@ namespace RealEstate_Dapper_UI.Controllers
                 var jsonData = await response.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDTO createCategoryDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDTO);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44382/api/Categories", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
