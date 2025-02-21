f5a19e5f3a1042cf51366ca0b0d2a81a5bc719cf
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-21 13:38:00 +0300

    Employee için api oluşturulması

---
+++ RealEstate_Dapper_Api/Repositories/EmployeeRepository/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using RealEstate_Dapper_Api.DTOs.EmployeeDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.EmployeeRepository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void CreateEmployee(CreateEmployeeDTO employeeDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultEmployeeDTO>> GetAllEmployeeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIDEmployeeDTO> GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(UpdateEmployeeDTO employeeDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/EmployeeRepository/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.DTOs.EmployeeDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.EmployeeRepository
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<ResultEmployeeDTO>> GetAllEmployeeAsync();
+        void CreateEmployee(CreateEmployeeDTO employeeDTO);
+        void DeleteEmployee(int id);
+        void UpdateEmployee(UpdateEmployeeDTO employeeDTO);
+        Task<GetByIDEmployeeDTO> GetEmployee(int id);
+    }
+}
