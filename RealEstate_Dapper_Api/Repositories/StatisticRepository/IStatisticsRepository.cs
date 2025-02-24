ee73ea31ca93fb219860858a85e9021f7299cd44
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-24 14:36:14 +0300

    İstatistik Controlleri ve Repository oluşturulması

---
+++ RealEstate_Dapper_Api/Repositories/StatisticRepository/IStatisticsRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_Dapper_Api.Repositories.StatisticRepository
+{
+    public interface IStatisticsRepository
+    {
+        int CategoryCount();
+        int ActiveCategoryCount();
+        int PassiveCategoryCount();
+        int ProductCount();
+        int ApartmentCount();
+        string EmployeeNameByMaxProductCount();
+    }
+}
