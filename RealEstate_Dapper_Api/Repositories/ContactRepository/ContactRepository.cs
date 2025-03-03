322fab750664a4eb25e89868b691b13946513d59
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-03 13:03:05 +0300

    Contact DTOları ve Repositoryleri eklendi

---
+++ RealEstate_Dapper_Api/Repositories/ContactRepository/ContactRepository.cs
@@ -0,0 +1,32 @@
+﻿using RealEstate_Dapper_Api.DTOs.ContactDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.ContactRepository
+{
+	public class ContactRepository : IContactRepository
+	{
+		public Task CreateContact(CreateContactDTO ContactDTO)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteContact(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<ResultContactDTO>> GetAllContactAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<GetByIDContactDTO> GetContact(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Last4ContactResultDTO>> GetLast4Contact()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ RealEstate_Dapper_Api/Repositories/ContactRepository/IContactRepository.cs
@@ -0,0 +1,14 @@
+﻿using RealEstate_Dapper_Api.DTOs.ContactDTOs;
+using System.Threading.Tasks;
+
+namespace RealEstate_Dapper_Api.Repositories.ContactRepository
+{
+	public interface IContactRepository
+	{
+		Task<List<ResultContactDTO>> GetAllContactAsync();
+		Task CreateContact(CreateContactDTO ContactDTO);
+		Task DeleteContact(int id);
+		Task<List<Last4ContactResultDTO>> GetLast4Contact();
+		Task<GetByIDContactDTO> GetContact(int id);
+	}
+}
