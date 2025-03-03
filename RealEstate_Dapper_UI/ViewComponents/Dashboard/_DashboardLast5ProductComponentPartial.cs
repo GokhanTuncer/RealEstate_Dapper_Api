ccaab31e31b63ff15920bb5d6bb237540ed2d89a
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-03 14:21:31 +0300

    Last5Product fix

--- RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardLast5ProductComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardLast5ProductComponentPartial.cs
@@ -15,7 +15,7 @@ namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
 		public async Task<IViewComponentResult> InvokeAsync()
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:44382/api/Products/Last5ProductsList");
+			var responseMessage = await client.GetAsync("https://localhost:44382/api/Products/Last5ProductList");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
