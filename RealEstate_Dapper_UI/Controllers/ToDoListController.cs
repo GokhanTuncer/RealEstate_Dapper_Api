badfbee5f62a89f7a61ec740e90d6e0f4788d831
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-03 14:55:55 +0300

    TDL controller ve partial'i

---
+++ RealEstate_Dapper_UI/Controllers/ToDoListController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.ToDoListDTOs;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+	public class ToDoListController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ToDoListController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var response = await client.GetAsync("https://localhost:44382/api/Categories");
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultToDoListDTO>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardToDoListComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.ToDoListDTOs;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
+{
+	public class _DashboardToDoListComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DashboardToDoListComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var response = await client.GetAsync("https://localhost:44382/api/ToDoLists");
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultToDoListDTO>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
