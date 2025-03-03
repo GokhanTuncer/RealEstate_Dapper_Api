7c7a15986f3bd31298606084a7d9a3a93c2458f3
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-03 14:30:34 +0300

    ToDoList repo. ve dtolar eklendi

---
+++ RealEstate_Dapper_Api/Repositories/ToDoListRepository/ITodoListRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.Repositories.ToDoListRepository
+{
+	public interface ITodoListRepository
+	{
+		Task<List<ResultCategoryDTO>> GetAllCategoryAsync();
+		void CreateCategory(CreateCategoryDTO categoryDTO);
+		void DeleteCategory(int id);
+		void UpdateCategory(UpdateCategoryDTO categoryDTO);
+		Task<GetByIDCategoryDTO> GetCategory(int id);
+	}
+}
