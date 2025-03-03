4dc0926730a9cc7e1a30be9a0bb816a62e3a93a2
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-03 14:31:17 +0300

    name fix

--- RealEstate_Dapper_Api/Repositories/ToDoListRepository/ITodoListRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ToDoListRepository/ITodoListRepository.cs
@@ -1,11 +1,13 @@
-﻿namespace RealEstate_Dapper_Api.Repositories.ToDoListRepository
+﻿using RealEstate_Dapper_Api.DTOs.ToDoListDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.ToDoListRepository
 {
 	public interface ITodoListRepository
 	{
-		Task<List<ResultCategoryDTO>> GetAllCategoryAsync();
-		void CreateCategory(CreateCategoryDTO categoryDTO);
-		void DeleteCategory(int id);
-		void UpdateCategory(UpdateCategoryDTO categoryDTO);
-		Task<GetByIDCategoryDTO> GetCategory(int id);
+		Task<List<ResultToDoListDTO>> GetAllToDoListAsync();
+		void CreateToDoList(CreateToDoListDTO ToDoListDTO);
+		void DeleteToDoList(int id);
+		void UpdateToDoList(UpdateToDoListDTO ToDoListDTO);
+		Task<GetByIDToDoListDTO> GetToDoList(int id);
 	}
 }
