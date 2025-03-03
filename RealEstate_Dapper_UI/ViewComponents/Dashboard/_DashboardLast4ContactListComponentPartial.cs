dd5c8b81ef242984d4f3203991bd614e2c2bf120
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-03 13:57:11 +0300

    UI'de contact için vc eklendi

---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardLast4ContactListComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
+{
+	public class _DashboardLast4ContactListComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DashboardLast4ContactListComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var response = await client.GetAsync("https://localhost:44382/api/Contacts/GetLast4Contact");
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
