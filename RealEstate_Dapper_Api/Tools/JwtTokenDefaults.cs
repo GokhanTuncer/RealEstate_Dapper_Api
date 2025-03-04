8fce7ad7bf92609a8f46a7212456424ecc436015
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-04 14:08:24 +0300

    Json Web Token için sınıflar eklendi

---
+++ RealEstate_Dapper_Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://localhost"; //Host'u dinleyen
+        public const string ValidIssuer = "https://localhost"; //Host'u Yayınlayan
+        public const string Key = "RealEstate...04032025Asp.NetCore8001*/*";   //Secret Key -- Sadece Eğitim için burada
+        public const int Expire = 5;
+
+    }
+}
