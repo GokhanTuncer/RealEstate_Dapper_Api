582557dc80fc231af3202f19b4faf8f6f1a3c0e2
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-06 14:29:16 +0300

    Programa'a emplyee için authorize etme eklendi

--- RealEstate_Dapper_UI/Controllers/EmployeeController.cs
+++ RealEstate_Dapper_UI/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RealEstate_Dapper_UI.DTOs.EmployeeDTOs;
 using System.Net.Http;
@@ -6,6 +7,7 @@ using System.Text;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
+    [Authorize]
     public class EmployeeController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
