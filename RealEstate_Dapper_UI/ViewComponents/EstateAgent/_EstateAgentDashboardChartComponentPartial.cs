be70010d7d2919428a296cd5f02863d13e28a241
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-10 15:01:33 +0300

    Chart bölümünün tamamlanması

--- RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardChartComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardChartComponentPartial.cs
@@ -1,9 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.EstateAgentDTOs;
 
 namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
 {
 	public class _EstateAgentDashboardChartComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke() { return View(); }
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _EstateAgentDashboardChartComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var response = await client.GetAsync("https://localhost:44382/api/EstateAgentChart");
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultEstateAgentDashboardChartDTO>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
 	}
 }
