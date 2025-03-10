4ff07f9c2bc87870ab1ca9f0ed36f3f9a53e333a
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-10 14:40:49 +0300

    Chart bölümünün apide tanımlanması

---
+++ RealEstate_Dapper_Api/DTOs/ChartDTOs/ResultChartDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_Dapper_Api.DTOs.ChartDTOs
+{
+	public class ResultChartDTO
+	{
+        public string City  { get; set; }
+        public int CityCount  { get; set; }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/EstateAgentRepository/DashboardRepository/ChartRepository/ChartRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.DTOs.CategoryDTOs;
+using RealEstate_Dapper_Api.DTOs.ChartDTOs;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.EstateAgentRepository.DashboardRepository.ChartRepository
+{
+	public class ChartRepository : IChartRepository
+	{
+		private readonly Context _context;
+
+		public ChartRepository(Context context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<ResultChartDTO>> Get5CityForChart()
+		{
+			string query = "Select Top(5) City,Count(*) as 'CityCount' From Product Group By City order by CityCount Desc";
+			using (var connection = _context.CreateConnection())
+			{
+				var values = await connection.QueryAsync<ResultChartDTO>(query);
+				return values.ToList();
+			}
+		}
+	}
+}
