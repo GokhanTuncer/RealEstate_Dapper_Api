e02ade950c30b6ecd1815be4d770194ddeab336c
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-10 13:26:48 +0300

    Api tarafından UIye çekmek üzere dashboard istatistik controller ve reponun hazırlanması

---
+++ RealEstate_Dapper_Api/Controllers/EstateAgentDashboardStatisticController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.EstateAgentRepository.DashboardRepository.StatisticRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EstateAgentDashboardStatisticController : ControllerBase
+	{
+		private readonly IStatisticRepository _statisticRepository;
+
+		public EstateAgentDashboardStatisticController(IStatisticRepository statisticRepository)
+		{
+			_statisticRepository = statisticRepository;
+		}
+
+		[HttpGet("ActiveCategoryCount")]
+		public IActionResult ActiveCategoryCount()
+		{
+			return Ok(_statisticsRepository.ActiveCategoryCount());
+		}
+		[HttpGet("ActiveCategoryCount")]
+		public IActionResult ActiveCategoryCount()
+		{
+			return Ok(_statisticsRepository.ActiveCategoryCount());
+		}
+		[HttpGet("ActiveCategoryCount")]
+		public IActionResult ActiveCategoryCount()
+		{
+			return Ok(_statisticsRepository.ActiveCategoryCount());
+		}
+		[HttpGet("ActiveCategoryCount")]
+		public IActionResult ActiveCategoryCount()
+		{
+			return Ok(_statisticsRepository.ActiveCategoryCount());
+		}
+	}
+}
---
+++ RealEstate_Dapper_Api/Repositories/EstateAgentRepository/DashboardRepository/StatisticRepository/IStatisticRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_Api.Repositories.EstateAgentRepository.DashboardRepository.StatisticRepository
+{
+	public interface IStatisticRepository
+	{
+		int ProductCountByEmployeeID(int id);   //Kullanıcının id'leri
+		int ProductCountByStatusTrue(int id);
+		int ProductCountByStatusFalse(int id);
+		int AllProductCount();
+	}
+}
