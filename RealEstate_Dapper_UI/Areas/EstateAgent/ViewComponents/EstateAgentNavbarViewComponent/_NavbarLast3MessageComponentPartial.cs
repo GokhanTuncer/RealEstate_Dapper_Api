2a566949b5ab56fed5dce50dd9f5b63744aec87a
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-11 14:02:28 +0300

    message DTO ve partialin ui'ye eklenmesi apinin consume edilmesi

---
+++ RealEstate_Dapper_UI/Areas/EstateAgent/ViewComponents/EstateAgentNavbarViewComponent/_NavbarLast3MessageComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.MessageDTOs;
+using RealEstate_Dapper_UI.Services;
+
+namespace RealEstate_Dapper_UI.Areas.EstateAgent.ViewComponents.EstateAgentNavbarViewComponent
+{
+    public class _NavbarLast3MessageComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILoginService _loginService;
+
+        public _NavbarLast3MessageComponentPartial(IHttpClientFactory httpClientFactory, ILoginService loginService)
+        {
+            _httpClientFactory = httpClientFactory;
+            _loginService = loginService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var id = _loginService.GetUserId;
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44382/api/Messages?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultInBoxMessageDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
--- RealEstate_Dapper_UI/DTOs/MessageDTOs/ResultInBoxMessageDTO.cs
+++ RealEstate_Dapper_UI/DTOs/MessageDTOs/ResultInBoxMessageDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace RealEstate_Dapper_Api.DTOs.MessageDTOs
+﻿namespace RealEstate_Dapper_UI.DTOs.MessageDTOs
 {
     public class ResultInBoxMessageDTO
     {
