40b7a981571f11954e386d8b1bdace7d397a944b
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-11 13:27:39 +0300

    Mesaj bölümü için apiye dto ve repository bölümü eklendi

---
+++ RealEstate_Dapper_Api/Repositories/MessageRepository/IMessageRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstate_Dapper_Api.DTOs.MessageDTOs;
+
+namespace RealEstate_Dapper_Api.Repositories.MessageRepository
+{
+    public interface IMessageRepository
+    {
+        Task<ResultInBoxMessageDTO> GetInBoxLast3MessageListByReceiver(int id);
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/MessageRepository/MessageRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace RealEstate_Dapper_Api.Repositories.MessageRepository
+{
+    public class MessageRepository
+    {
+    }
+}
