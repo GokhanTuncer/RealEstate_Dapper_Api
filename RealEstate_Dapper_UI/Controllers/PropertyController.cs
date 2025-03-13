739ce6aaa88328f0556613d107c6382baf927773
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-13 16:31:40 +0300

    ilana özgü detayların listelenmesi için controller düzenlemesi ve index eklenmesi

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.ProductDTOs;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
     public class PropertyController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PropertyController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44382/api/Products/ProductListWithCategory");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+        [HttpGet]
+        public async Task<IActionResult> PropertySingle()
+        {
+			return View();
+		}
+
     }
 }
+
