00ab3cc890a7f1046fab59cb2e9d7b9cc3bc8736
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-17 14:10:30 +0300

    Ilan sayfası resim kısmının component'inin eklenmesi

---
+++ RealEstate_Dapper_UI/DTOs/PropertyImageDTOs/PropertyImageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_Dapper_UI.DTOs.PropertyImageDTOs
+{
+    public class PropertyImageDTO
+    {
+        public int ProductImageID { get; set; }
+        public string ImageURL { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/PropertySingle/_PropertySliderComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.PropertyImageDTOs;
+
+namespace RealEstate_Dapper_UI.ViewComponents.PropertySingle
+{
+    public class _PropertySliderComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PropertySliderComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44382/api/Products/ProductImages?id=");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<PropertyImageDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
