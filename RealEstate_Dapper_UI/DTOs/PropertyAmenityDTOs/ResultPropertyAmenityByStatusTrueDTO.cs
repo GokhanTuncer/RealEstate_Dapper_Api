98df5d6adb2af5245bdf4695b20a9b455cc16b49
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-17 16:04:32 +0300

    Ilan Özellik bölümünün Api tarafından consume edilmesi

---
+++ RealEstate_Dapper_UI/DTOs/PropertyAmenityDTOs/ResultPropertyAmenityByStatusTrueDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_Dapper_UI.DTOs.PropertyAmenityDTOs
+{
+    public class ResultPropertyAmenityByStatusTrueDTO
+    {
+        public int PropertyAmenityID { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/PropertySingle/_PropertyAmenityStatusToTrueByPropertyIDComponentPartial.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.PropertyAmenityDTOs;
+
+namespace RealEstate_Dapper_UI.ViewComponents.PropertySingle
+{
+    public class _PropertyAmenityStatusToTrueByPropertyIDComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _PropertyAmenityStatusToTrueByPropertyIDComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44382/api/PropertyAmenities?id=1");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPropertyAmenityByStatusTrueDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+}
