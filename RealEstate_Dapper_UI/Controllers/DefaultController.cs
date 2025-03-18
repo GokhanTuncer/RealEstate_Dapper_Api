155239ba3421be7a816dd83911ef98790cde593c
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-18 14:20:14 +0300

    default controllere ekleme

--- RealEstate_Dapper_UI/Controllers/DefaultController.cs
+++ RealEstate_Dapper_UI/Controllers/DefaultController.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using NuGet.Configuration;
+using RealEstate_Dapper_UI.DTOs.CategoryDTOs;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            
+        }
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public PartialViewResult PartialSearch()
+        public async Task<PartialViewResult> PartialSearch()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44382/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+                return PartialView(values);
+            }
             return PartialView();
         }
 
